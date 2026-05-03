154d901cccf4fd02e1629bbb18e7b4adab8158cc
Lulu <lulu.masafierro1@gmail.com>
2026-05-03 11:05:37 -0400

    Se finaliza la página Index.html

--- m2_app_clima/assets/js/script.js
+++ m2_app_clima/assets/js/script.js
@@ -7,3 +7,13 @@ const carousel = new bootstrap.Carousel(myCarousel, {
   interval: 4000,
   wrap: true
 });
+
+console.log("Sección Main");
+
+document.getElementById("btnTop").addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
